0a392cdafe611d8cc8b43d01332d2c8fe4b85571
Anatoly Kovalev <inatoley@gmail.com>
2015-07-01 22:00:28 +0300

    s

---
+++ XMLParserExampleSwift/Product.swift
@@ -0,0 +1,19 @@
+//
+//  Product.swift
+//  XMLParserExampleSwift
+//
+//  Created by Fenkins on 30/06/15.
+//  Copyright (c) 2015 Fenkins. All rights reserved.
+//
+
+import Foundation
+
+class Product {
+    var productName:String = ""
+    var productDescription:String = ""
+
+init(name:String, description:String) {
+    self.productName = name
+    self.productDescription = description
+    }
+}
---
+++ XMLParserExampleSwift/ProductsParser.swift
@@ -0,0 +1,14 @@
+//
+//  ProductsParser.swift
+//  XMLParserExampleSwift
+//
+//  Created by Fenkins on 01/07/15.
+//  Copyright (c) 2015 Fenkins. All rights reserved.
+//
+
+import Foundation
+
+class ProductParser:NSObject, NSXMLParserDelegate {
+    let filePathString:String = "productsList.xml"
+    
+}
