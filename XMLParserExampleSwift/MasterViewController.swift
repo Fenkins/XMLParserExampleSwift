085e5b584886d529fb85bdf585260e7002926024
Anatoly Kovalev <inatoley@gmail.com>
2015-07-02 15:34:10 +0300

    yea wow

--- XMLParserExampleSwift/MasterViewController.swift
+++ XMLParserExampleSwift/MasterViewController.swift
@@ -8,10 +8,63 @@
 
 import UIKit
 
-class MasterViewController: UITableViewController {
+class MasterViewController: UITableViewController, NSXMLParserDelegate {
 
     var objects = [AnyObject]()
 
+    var parser = NSXMLParser()
+    var productArray = NSMutableArray()
+    var elements = NSMutableDictionary()
+    var element = NSString()
+    var productName = NSMutableString()
+    var productDescrition = NSMutableString()
+    
+    func execParse() {
+        productArray = []
+        var url:NSURL = NSURL(fileURLWithPath: (NSBundle.mainBundle().pathForResource("productsList", ofType: ".xml"))!)!
+        parser = NSXMLParser(contentsOfURL: url)!
+        parser.delegate = self
+        parser.shouldProcessNamespaces = false
+        parser.shouldReportNamespacePrefixes = false
+        parser.shouldResolveExternalEntities = false
+        parser.parse()
+        
+    }
+    
+    func parser(parser: NSXMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [NSObject : AnyObject]) {
+        element = elementName
+        
+        // Instantiating
+        if (element as NSString).isEqualToString("Product") {
+            elements = NSMutableDictionary.alloc()
+            elements = [:]
+            productName = NSMutableString.alloc()
+            productName = ""
+            productDescrition = NSMutableString.alloc()
+            productDescrition = ""
+        }
+        
+    }
+    
+    func parser(parser: NSXMLParser, foundCharacters string: String?) {
+        if element.isEqualToString("Name") {
+            productName.appendString(string!)
+        } else if element.isEqualToString("Description") {
+            productDescrition.appendString(string!)
+        }
+    }
+
+    func parser(parser: NSXMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        if (elementName as NSString) .isEqualToString("Product") {
+            if productName != "" {
+                elements.setObject(productName, forKey: "Product Name")
+            }
+            if productDescrition != "" {
+                elements.setObject(productDescrition, forKey: "Product Description")
+            }
+            productArray.addObject(elements)
+        }
+    }
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -24,6 +77,9 @@ class MasterViewController: UITableViewController {
 
         let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
         self.navigationItem.rightBarButtonItem = addButton
+        
+        
+        execParse()
     }
 
     override func didReceiveMemoryWarning() {
@@ -55,6 +111,8 @@ class MasterViewController: UITableViewController {
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        println("Products", "\(productArray.count)")
+        return productArray.count
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
@@ -80,5 +138,6 @@ class MasterViewController: UITableViewController {
     }
 
 
+    
 }
 
